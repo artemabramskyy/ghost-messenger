43f75c17f196423f23fc3e528ad101f81a9244d2
artemabramskyy <a.abramskyy@gmail.com>
2024-08-16 21:04:10 +0300

    Added server for client

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,35 @@
-console.log('Hello Messege');
+import express from 'express';
+import path from 'path';
+
+// Routes
+// import {} from 'routes';
+
+// Middlewares
+// import {} from 'middlewares';
+
+const PORT = 4000;
+const BODY_PARSER_LIMIT = '2mb';
+const API_BASE_PATH = '/api';
+const API_VERSION = 'v1';
+const BASE_URL = `${API_BASE_PATH}/${API_VERSION}`;
+
+const app = express();
+const staticPath = path.join(__dirname, '../../dist/client');
+
+app.use(express.static(staticPath));
+app.use(express.json({ limit: BODY_PARSER_LIMIT }));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// app.use(`${baseUrl}/auth`, authRoute);
+
+app.get('*', (req, res) => {
+  res.sendFile(`${staticPath}/index.html`);
+});
+
+app.listen(PORT, () => {
+  console.log(`🤖: Ghost Messenger server listening on port: ${PORT}`);
+});
